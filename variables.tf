c11cda4eb8123a2550194dd758a8881c2cf29be5
Elsherif007 <melsherif695@gmail.com>
2022-06-04 04:15:36 +0200

    f

--- variables.tf
+++ variables.tf
@@ -3,6 +3,8 @@ variable "resource_group_name_prefix" {
   description   = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
 }
 
+
+
 variable "resource_group_location" {
   default       = "eastus"
   description   = "Location of the resource group."
